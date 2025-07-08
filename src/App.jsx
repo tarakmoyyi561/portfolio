00972d8767e00d96f163df4974df7d41c84d7497
tarakmoyyi561 <tarakmoyyi@gmail.com>
2025-07-08 14:52:53 +0530

    Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -23,6 +24,7 @@ function App() {
         <Projects/>
         <Contacts/>
       </div>
+      <Analytics />
     </div>
   )
 }
